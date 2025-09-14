d047bd953c8f26995631a553d800f62ad6c94f38
TylerCarricato <127124994+TylerCarricato@users.noreply.github.com>
2025-09-14 16:17:49 -0400

    fixed syntax error

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ function sendMail(){
         message : document.getElementById("message").value,
     }
 
-    email.js.send("service_u1w4xwp", "template_5kmjt1b", parms).then(alert("Email Sent"))
+    emailjs.send("service_u1w4xwp", "template_5kmjt1b", parms).then(alert("Email Sent"))
 }
